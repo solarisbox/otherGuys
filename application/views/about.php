e8717bb236a945e11019f16537766509e5114edd
Dylan Hernandez <dylan.hernandez@mohawkcollege.ca>
2016-11-01 14:54:43 -0400

    First Commit - About Page Change
    Changes description a little

--- application/views/about.php
+++ application/views/about.php
@@ -11,9 +11,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <div id="wrap">
 	<h1>About Ephemeral</h1>
 	<div id="body">
-		<p>About Ephemeral The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
+		<div class="row">
+			<div class="col-xs-12">
+				About Ephemeral
+			</div>
+			<div class=col-xs-12>
+				Insert description here!
+			</div>
+		</div>
 	</div>
 </div>
 <?
