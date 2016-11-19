883bd14ce019e8b6b28ea6abdcaafec337f5c850
Dylan Hernandez <dylan.hernandez@mohawkcollege.ca>
2016-11-19 18:38:23 -0500

    Added CSS Override file
    If you want to change an element from bootstrap for whatever reason do
    it in this file.
    
    I turned off the rounded corners in bootstrap for a sharper look, if you
    want to change it back just comment out the code block

--- application/views/header.php
+++ application/views/header.php
@@ -2,16 +2,13 @@
 <html lang="en">
  <head>
  <title>Ephemeral</title>
- <meta name="description" content="Privacy, post highly classified information">
- <meta name="keywords" content="Secret, Privacy, Wikileaks, Government, Highly Confidential">
- <meta http-equiv="content-type" content="text/html;charset=UTF-8">
- <link rel="stylesheet" type="text/css" href="<?= assetUrl(); ?>css/bootstrap-theme.min.css">
- <link rel="stylesheet" type="text/css" href="<?= assetUrl(); ?>css/bootstrap.min.css">
- <link rel="stylesheet" type="text/css" href="<?= assetUrl(); ?>css/custom.css">
-
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
- 
- 
-
+    <meta name="description" content="Privacy, post highly classified information">
+    <meta name="keywords" content="Secret, Privacy, Wikileaks, Government, Highly Confidential">
+    <meta http-equiv="content-type" content="text/html;charset=UTF-8">
+    <link rel="stylesheet" type="text/css" href="<?= assetUrl(); ?>css/bootstrap-theme.min.css" />
+    <link rel="stylesheet" type="text/css" href="<?= assetUrl(); ?>css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="<?= assetUrl(); ?>css/custom.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <link rel="stylesheet" href="<?= assetUrl(); ?>css/override.css" type="text/css" />
  </head>
  <body>
