77ee747ca9eec5bbe61fea5f66b6038683d920c5
Dylan Hernandez <dylan.hernandez@mohawkcollege.ca>
2016-11-21 09:19:58 -0500

    Thread Start Front-end Update
    Updated front-end for thread creation

--- application/views/start.php
+++ application/views/start.php
@@ -6,8 +6,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<meta charset="utf-8">
 	<title>Start New Thread</title>
 </head>
-<body>
-<div id="wrap">
+<body class="home">
+<!--
+
 	<section class="container">
 		<div class="row">
 			<div id="results"></div>
@@ -20,15 +21,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			            <input type="text" class="form-control" id="title" placeholder="Title Thread">
 			        </div>
 			    </div>
-		          
+
 		        <div class="form-group">
 			        <label for="envan" class="control-label col-xs-2">Post</label>
 			        <div style="height:100%;"class="col-xs-10">
 			          <textarea type="text" rows="10" style="height:100%;" class="form-control" id="message" placeholder="Thread Post..."></textarea>
 			        </div>
 		        </div>
-			         
-			    		
+
+
 			    <div class="form-group">
 			        <div class="col-xs-offset-2 col-xs-10">
 			            <label class="radio-inline">
@@ -38,7 +39,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					      <input type="radio" name="optradio">Minutes - 60
 					    </label>
 					    <label class="radio-inline">
-					      <input type="radio" name="optradio">Never 
+					      <input type="radio" name="optradio">Never
 					    </label>
 			        </div>
 			    </div>
@@ -51,23 +52,71 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			</form>
 	      </div>
 
-	      
+
 	  </div>
 	</section>
-</div>
 
+-->
+<div class="container">
+	<div class="row">
+		<div class="col-xs-12">
+			<div id="results"></div>
+		</div>
+		<div class="col-xs-12 col-sm-8 col-sm-offset-2">
+			<form class="form-horizontal" id="new-thread">
+				<div class="panel panel-eph">
+					<div class="panel-heading">
+						<h3>Start New Thread</h3>
+					</div>
+					<div class="panel-body">
+						<div class="row">
+							<div class="col-xs-12">
+								<div class="form-group">
+									<label for="title" class="control-label">Title</label>
+									<input type="text" class="form-control" id="title" placeholder="Title Thread">
+								</div>
+							</div>
+							<div class="col-xs-12">
+								<div class="form-group">
+									<label for="message" class="control-label">Post</label>
+									<textarea type="text" rows="10" style="height:100%;" class="form-control" id="message" placeholder="Thread Post..."></textarea>
+								</div>
+							</div>
+							<div class="col-xs-12">
+								<div class="form-group">
+									<label class="radio-inline">
+										<input type="radio" name="optradio">Minutes - 5
+									</label>
+									<label class="radio-inline">
+										<input type="radio" name="optradio">Minutes - 60
+									</label>
+									<label class="radio-inline">
+										<input type="radio" name="optradio">Never
+									</label>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="panel-footer">
+						<div class="row">
+							<div class="col-xs-12">
+								<button type="button" class="btn btn-primary btn-block" onclick="post();">Submit</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
 <script type="text/javascript">
 	function post(){
 		var title = $('#title').val();
 		var message = $('#message').val();
-
-		$.post('message.php' , {posttitle:title,postbody:message},
-			function(data)
-			{
+		$.post('message.php' , { posttitle:title, postbody:message }, function(data) {
 				// $('#results').html(data);
 				window.location.href = "index.php?/Thread/display/" + data;
 		});
-
 		//redirect.location = 	
 	}
 </script>
