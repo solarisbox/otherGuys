eebd1146b6bf7e90e8de67388b34ad8231f66ba0
Sal <compuboxt@gmail.com>
2016-11-22 17:25:19 -0500

    Added New Contact List Page, and List Panel
    Added template for:
    -New Contact List
    -Individual List Panel

---
+++ application/controllers/ListPanel.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class ListPanel extends MY_Controller {
+
+	var $TPL;
+
+	public function __construct()
+	{
+	parent::__construct();
+	// Your own constructor code
+	}
+
+	private function display()
+	{
+	$this->template->show('list_panel', $this->TPL);
+	}
+
+	public function index()
+	{
+
+	$this->display();
+
+	$user = $this->session->userdata('user');
+	echo $user;
+
+	}
+
+}//End of userPanel
---
+++ application/views/list_panel.php
@@ -0,0 +1,44 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Ephemeral Board</title>
+</head>
+<body class="home">
+	<div class="container">
+		<div class="jumbotron force-transparent">
+			<h1 class="text-center">Ephemeral</h1>
+		</div>
+		<div class="row">
+			<div class="col-xs-12">
+				<div class="panel panel-default force-transparent bordered">
+					<div class="panel-body">
+						<h2>User Name Here</h2>
+						<div class="col-md-2 col-lg-2" id="leftCol">
+							<ul class="nav nav-stacked user-profile" id="sidebar">
+							<li><a href="<?= base_url(); ?>index.php?/UserPanel">Control Panel</a></li>
+							<li><a href="<?= base_url(); ?>index.php?/ProfileSettings">Profile Settings</a></li>
+							<li><a href="<?= base_url(); ?>index.php?/ContactList">Contact List</a></li>
+							<li><a href="<?= base_url(); ?>index.php?/UserPosts">All Posts</a></li>
+							<li><a href="<?= base_url(); ?>index.php?/UserThreads">Threads</a></li>
+							</ul>
+						</div><!--left-->
+						<div class="col-md-10 col-lg-10">
+							<h2>Name of Lists</h2>
+							<!--This page will use Ajax for live search-->
+							<form method = "POST" id = "searchList centered" action = "<?php echo base_url() . 'index.php?/' ?>">
+								<div class= "row col-lg-12 col-md-12 col-sm-6">
+									<div class = "form-group">
+										<label for = "members">Search Members:</label>
+										<input type = "text" id = "members" name = "members" class = "form-control default-search" />
+									</div>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
